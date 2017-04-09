4a4d3bbead1fab30044673b5b89fda342bc990f5
zhenhao goh <kenneth.gzhao@outlook.com>
2017-04-09 13:42:01 +0800

    homework complete

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -1,5 +1,8 @@
+const mongoose = require('mongoose')
+const express = require('express')
+const app = express()
 const Todo = require('../models/todo')
-
+const router = express.Router()
 // TODO. import express and create a Router, replace the methods below with routes e.g.
 // router.post('/', function(req,res) => {
 //  Todo.create(req.body, function (err, todo) {
@@ -7,51 +10,81 @@ const Todo = require('../models/todo')
 //  }
 // })
 
-function create (params) {
-  Todo.create(params, function (err, todo) {
-    if (err) {
-      console.log(err)
-      return
-    }
-    console.log(todo)
+router.get('/', function (req, res) {
+  Todo.find({
+    completed: false
+  }, function (err, todos) {
+    if (err) throw err
+    // console.log(todos)
+    res.render('homepage', {
+      allTodos: todos
+    })
   })
-}
+})
 
-function list () {
+router.post('/create', function (req, res) {
+  // console.log(req.body);
+  var newtodo = new Todo(req.body)
+  newtodo.save(function (err, data) {
+    if (err) throw err
+    // console.log(data)
+    // console.log(data._id);
+    Todo.find({
+      _id: data.id
+    }, function (err, todos) {
+      if (err) throw err
+      res.render('homepage', {
+        allTodos: todos
+      })
+    })
+  })
+})
+
+router.get('/listall', function (req, res) {
   Todo.find({}, function (err, todos) {
-    if (err) {
-      console.log(err)
-      return
-    }
-    console.log(todos)
+    if (err) throw err
+    res.render('homepage', {
+      allTodos: todos
+    })
   })
-}
+})
 
-function show (id) {
-  Todo.findById(id, function (err, todo) {
+router.post('/findid', function (req, res) {
+  var id = req.body.idSearch
+  Todo.find({
+    _id: id
+  }, function (err, todos) {
     if (err) return console.log(err)
-    console.log(todo)
+    res.render('homepage', {
+      allTodos: todos
+    })
   })
-}
+})
 
-function update (id, params) {
-  Todo.findOneAndUpdate({ _id: id }, params, function (err, todo) {
-    if (err) console.log(err)
-    console.log(todo)
+router.post('/update', function (req, res) {
+  // console.log(req.body)
+  Todo.find({
+    _id: req.body.idSearch
+  }, function (err, data) {
+    if (err) throw err
+    data[0].name = req.body.name
+    data[0].description = req.body.description
+    data[0].completed = req.body.completed
+    data[0].save()
+    //  console.log(data)
+    res.render('homepage', {
+      allTodos: data
+    })
   })
-}
+})
 
-function destroy (id) {
-  Todo.findOneAndRemove({ _id: id }, function (err) {
-    if (err) console.log(err)
-    console.log('User deleted!')
+router.post('/remove', function (req, res) {
+  // console.log(req.body)
+  Todo.findOneAndRemove({_id: req.body.remove}, function(err, data){
+    // console.log(data)
   })
-}
+  res.redirect('/')
+})
 
-module.exports = {
-  create,
-  list,
-  show,
-  update,
-  destroy
-}
+
+module.exports = router
--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 const mongoose = require('mongoose')
+const dbURI = 'mongodb://localhost/todo-list'
+const port = 4000
 const todosController = require('./controllers/todos_controller')
-
-mongoose.connect('mongodb://localhost/todo-list')
+const express = require('express')
+// console.log(express)
+const app = express()
+// console.log(app) an object with various functions
+const Todo = require('./models/todo')
 mongoose.Promise = global.Promise
 
+// installed ovrride method for deleting post
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
+
+// to server js css file images in a folder named public which i might create with an absolute path string
+var path = require('path')
+// console.log(path)
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
 // TODO. include express and body-parser, plugin in the todos controller and start listening
+// this is where your route start
+app.set('view engine', 'ejs')
+
+// setting the layout structure
+var ejsLayouts = require('express-ejs-layouts')
+app.use(ejsLayouts)
+
+// handle the post request
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
+if (!mongoose.connection.db) mongoose.connect(dbURI)
+
+app.use('/', todosController)
+
+app.listen(port, function () {
+  console.log('connected to port: ' + port)
+})
