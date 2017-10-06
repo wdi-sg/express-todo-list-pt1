627293c92c342f7e195edbe39b411ee68758fe55
Prima Aulia Gusta <primAulia@gmail.com>
2017-10-06 21:53:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 const todosController = require('./controllers/todos_controller')
 
-mongoose.connect('mongodb://localhost/todo-list')
+mongoose.connect('mongodb://localhost/todo-list', {
+  useMongoClient: true
+})
 mongoose.Promise = global.Promise
 
 // TODO. include express and body-parser, plugin in the todos controller and start listening
